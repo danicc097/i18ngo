8000e231212f112e1d3ea22a8ba9cbd931f439eb
danicc097 <danicc097@gmail.com>
2024-09-19 09:23:07 +0200

    expression validator with variables

--- validator/expression.go
+++ validator/expression.go
@@ -1,12 +1,33 @@
 package validator
 
 import (
+	"fmt"
+	"go/ast"
 	"go/parser"
 	"go/token"
+	"slices"
 )
 
-func ValidateGoExpression(expr string) error {
+func ValidateCustomTemplate(expression string, variables []string) error {
 	fs := token.NewFileSet()
-	_, err := parser.ParseExprFrom(fs, "", expr, 0)
+	expr, err := parser.ParseExprFrom(fs, "", expression, 0)
+	if err != nil {
+		return fmt.Errorf("invalid go expression: %w", err)
+	}
+
+	if len(variables) == 0 {
+		return nil
+	}
+
+	ast.Inspect(expr, func(n ast.Node) bool {
+		if ident, ok := n.(*ast.Ident); ok {
+			if !slices.Contains(variables, ident.Name) {
+				err = fmt.Errorf("unknown variable used in expression: %s", ident.Name)
+				return false
+			}
+		}
+		return true
+	})
+
 	return err
 }
--- validator/expression_test.go
+++ validator/expression_test.go
@@ -6,25 +6,30 @@ import (
 	"github.com/danicc097/i18ngo/validator"
 )
 
-// NOTE: already extensively tested in stdlib.
-func TestValidateGoExpression(t *testing.T) {
+func TestValidateCustomTemplate(t *testing.T) {
 	tests := []struct {
 		name      string
 		expr      string
+		variables []string
 		shouldErr bool
 	}{
-		{"ValidExpression1", "count == 0", false},
-		{"ValidExpression2", "name != \"\"", false},
-		{"InvalidExpression", "invalid-expression(", true},
-		{"ValidExpression3", "age > 18 && count != 0", false},
-		{"InvalidExpression2", "count !@ 0", true},
+		{"ValidExpression1", "count == 0", []string{"count"}, false},
+		{"ValidExpression2", "name != \"\"", []string{"name"}, false},
+		{"ValidExpression3", "age > 18 && count != 0", []string{"age", "count"}, false},
+
+		{"InvalidVariable1", "unknown == 0", []string{"count"}, true},
+		{"InvalidVariable2", "name != \"\" && id > 0", []string{"name"}, true},
+
+		// Syntax errors (already extensively tested in std lib)
+		{"InvalidExpression", "invalid-expression(", []string{}, true},
+		{"InvalidSyntax2", "count !@ 0", []string{"count"}, true},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			err := validator.ValidateGoExpression(tt.expr)
+			err := validator.ValidateCustomTemplate(tt.expr, tt.variables)
 			if (err != nil) != tt.shouldErr {
-				t.Errorf("ValidateGoExpression() error = %v, wantErr %v", err, tt.shouldErr)
+				t.Errorf("ValidateCustomTemplate() error = %v, wantErr %v", err, tt.shouldErr)
 			}
 		})
 	}
